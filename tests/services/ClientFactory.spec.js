5ac57b5331af99b17f59d1a2bbc6f080edecd3cc
Kianek <kallridge92@gmail.com>
2021-04-29 07:33:21 -0700

    Add test

--- tests/services/ClientFactory.spec.js
+++ tests/services/ClientFactory.spec.js
@@ -1,1 +1,12 @@
 const expect = require('chai').expect;
+const compileModel = require('../../functions/mongoose-helpers').compileModel;
+const ClientFactory = require('../../services/ClientFactory');
+const ClientSchema = require('../../models/ClientSchema');
+const Client = compileModel('Client', ClientSchema);
+
+describe('ClientFactory', () => {
+  it('#createClient instantiates a Client model', () => {
+    const factory = new ClientFactory();
+    expect(factory.createClient()).to.be.a(typeof Client);
+  });
+});
