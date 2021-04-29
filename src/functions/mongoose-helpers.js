78b35b75d2f4ed2ee9309aaf48c41dab2e6b16cd
Kianek <kallridge92@gmail.com>
2021-04-29 15:15:40 -0700

    Remove unused file

--- src/functions/mongoose-helpers.js
+++
@@ -1,6 +0,0 @@
-// TODO: this might be unnecessary
-const mongoose = require('mongoose');
-
-const compileModel = (name, schema) => mongoose.model(name, schema);
-
-module.exports = { compileModel };
